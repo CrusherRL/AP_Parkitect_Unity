7d97940ca8df40530e2c53471e8fdce3db89a736
DESKTOP-LBMSE4L\Crusher <danieldh@hotmail.de>
2026-06-26 23:11:35 +0200

    feature(1.6.0): added EnergyLink

--- Archipelago Window/Assets/Editor/CreateAssetBundles.cs
+++ Archipelago Window/Assets/Editor/CreateAssetBundles.cs
@@ -6,6 +6,8 @@ public class CreateAssetBundles
     [MenuItem("Bundler/Build Asset Bundles")]
     static void BuildAllAssetBundles()
     {
-        BuildPipeline.BuildAssetBundles("Assets/Dist", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+        BuildPipeline.BuildAssetBundles("Assets/Dist/Windows", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+        BuildPipeline.BuildAssetBundles("Assets/Dist/MacOS", BuildAssetBundleOptions.None, BuildTarget.StandaloneOSX);
+        BuildPipeline.BuildAssetBundles("Assets/Dist/Linux", BuildAssetBundleOptions.None, BuildTarget.StandaloneLinux64);
     }
 }
